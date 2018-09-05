48e00c741a94c07e4cf8056811bbada44c0a07e1
Hveden <tobias.ciesla@hotmail.com>
2018-09-05 22:44:37 +0200

    l

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 function start(){
   ble.scan([], 5, function(device) {
-    console.log(JSON.stringify(device));
+    document.getElementById("hi").appendChild(JSON.stringify(device));
 }, failure);
 }
 
 function conn(){
-  
+
 }
